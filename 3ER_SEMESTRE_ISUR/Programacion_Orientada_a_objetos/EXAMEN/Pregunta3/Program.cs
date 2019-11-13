ede93e33ce4406ce50e4e2c3730cfdf7c7229bd9
Christian <mxcat.xxtremxx@gmail.com>
2019-11-12 21:00:25 -0500

    Signed-off-by: Christian <mxcat.xxtremxx@gmail.com>

---
+++ 3ER_SEMESTRE_ISUR/Programacion_Orientada_a_objetos/EXAMEN/Pregunta3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Pregunta3
+{
+    class Taller{
+        public Automovil[] AutomovilesTaller;
+        public Taller(int cantiaforo){
+            AutomovilesTaller=new Automovil[cantiaforo];
+        }
+        public Automovil this[int x]
+        {
+            get { return AutomovilesTaller[x]; }
+            set { AutomovilesTaller[x]=value; }
+        }
+    }
+
+    class Automovil{
+        private string _marca;
+        private int _anio;
+        private string _modelo;
+
+        public LLanta[] LLantasAutomovil;
+        public Motor MotorAutomovil;
+        public Puerta[] PuertasAutomovil;
+
+        public Automovil(LLanta[] llantas, Motor motor,Puerta[] puertas){
+            LLantasAutomovil=llantas;
+            MotorAutomovil=motor;
+            PuertasAutomovil=puertas;
+
+        }
+        public string Marca {get{return _marca;}set{_marca=value;}}
+        public int Anio {get{return _anio;}set{_anio=value;}}
+        public string Modelo {get{return _modelo;}set{_modelo=value;}}
+    }
+    class LLanta{
+        private string _marca;
+        private string _modelo;
+        private int _tamaniopulgadas;
+
+        public LLanta(){
+
+        }
+        public string Marca
+        {
+            get { return _marca; }
+            set { _marca = value; }
+        }
+        public string Modelo
+        {
+            get { return _modelo; }
+            set { _modelo = value; }
+        }
+        public int Tamaniopulgadas
+        {
+            get { return _tamaniopulgadas; }
+            set { _tamaniopulgadas = value; }
+        }
+        
+        
+        
+    }
+    class Motor{
+
+        private string _numeroserie;
+        private int _cilindros;
+        public Motor(){
+
+        }
+        public string Numeroserie
+        {
+            get { return _numeroserie; }
+            set { _numeroserie = value; }
+        }
+        public int Cilindros
+        {
+            get { return _cilindros; }
+            set { _cilindros = value; }
+        }
+        
+        
+    }
+    class Puerta{
+        private string _tipo;
+
+        public Puerta(){
+
+        }
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
+        
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ 3ER_SEMESTRE_ISUR/Programacion_Orientada_a_objetos/EXAMEN/Pregunta4/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pregunta4
+{
+    interface IEquatable<Ingrediente>
+    {
+        bool Equals(Ingrediente t);
+    }
+    interface IComparable<Ingrediente>
+    {
+        int CompareTo(Ingrediente T);
+    }
+
+    public delegate void CriterioOrdenamiento();
+    class Pastel{
+        private List<Ingrediente> ListaIngredientes=new List<Ingrediente>();
+        private string _strNombre;
+        public string Nombre
+        {
+            get { return _strNombre; }
+            set { _strNombre = value; }
+        }
+        public void AgregarIngrediente(Ingrediente ingre){
+            ListaIngredientes.Add(ingre);
+        }
+        public bool EliminarIngrediente(Ingrediente ingre){
+            if(ingre != null){
+                ListaIngredientes.Remove(ingre);
+                return true;
+            }
+            return false;
+        }
+        ~Pastel(){
+
+        }
+
+        public override string ToString(){
+            return $"Este es una pastel {this.Nombre}";
+        }
+
+        public void Ascendente(){
+            
+        }
+
+        public void Descendente(){
+            
+        }
+        public CriterioOrdenamiento Ctr;
+
+        public void OrdenarIngrediente(CriterioOrdenamiento ctr){
+            Ctr=new CriterioOrdenamiento(ctr);
+            Ctr();
+        }
+
+
+
+        
+    }
+    class Ingrediente:IEquatable<Ingrediente>,IComparable<Ingrediente>{
+
+        private string _strNombre;
+        private double _dblCantidad;
+        public string Nombre
+        {
+            get { return _strNombre; }
+            set { _strNombre = value; }
+        }
+        public double Cantidad
+        {
+            get { return _dblCantidad; }
+            set { _dblCantidad = value; }
+        }
+        
+
+        public bool Equals(Ingrediente Ingre)
+        {
+            
+            if (Ingre == null || GetType() != Ingre.GetType())
+            {
+                return false;
+            }
+            
+            
+            return true;
+        }
+        public int CompareTo(Ingrediente Ingre){
+            return 1;
+        }
+        
+        public override string ToString(){
+            return $"El Objeto ingrediente es {this.Nombre} y hay una cantidad {this.Cantidad}";
+        }
+
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Pastel p1=new Pastel();
+            p1.OrdenarIngrediente(p1.Ascendente);
+        }
+    }
+}
